c9ddeac9fe8eadf67457039baba04b785e90eb36
ravi <samcollins9899@gmail.com>
2022-12-29 00:34:06 +0530

    added image viewer

--- src/components/microComponents/TimeTable.jsx
+++ src/components/microComponents/TimeTable.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { Link } from "react-router-dom";
+import image1 from '../../assets/classs1.jpeg';
+
+import ImageViewer from "react-simple-image-viewer";
+
+
 const TimeTable = () => {
+
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+  
+  const images = [
+    "https://i.ibb.co/yRzMngr/cse3rd-year.jpg",
+    "https://i.ibb.co/F3NbsVV/TIME-TABLE-FOR-CSE-4th-YEAR.jpg",
+    "https://i.ibb.co/FVj6nrk/TIME-TABLE-FOR-CSE-2nd-YEAR-1.jpg",
+
+  ];
+  const openImageViewer = useCallback((index) => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  }, []);
+
+
+  
+
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
+
   return (
     <>
       <header className="title-header">
         <div className="title-header-intro">
-          <p> <Link className="link-text" to="/" >Home </Link><span className="bold-text">/ Academics</span></p>
+          <p> <Link className="link-text" to="/" >Home </Link>/ <Link className="link-text" to="/academics" >Academics </Link> <span className="bold-text">/ Class Schedule</span></p>
         </div>
 
         <div className="tle-gutter">
@@ -34,9 +62,23 @@ const TimeTable = () => {
           
 
          <div className="main-text-links">
-           <p className="text-underline"> <Link className="link-text" to="/academics/programme" >4<sup>th</sup> Year CSE</Link></p>
-           <p className="text-underline"> <Link className="link-text" to="/academics/programme" >3<sup>rd</sup> Year CSE</Link></p>
-           <p className="text-underline"> <Link className="link-text" to="/academics/programme" >2<sup>nd</sup> Year CSE</Link></p>
+           {isViewerOpen && (
+        <ImageViewer
+          src={images}
+          currentIndex={currentImage}
+          onClose={closeImageViewer}
+          disableScroll={false}
+          backgroundStyle={{
+            backgroundColor: "rgba(0,0,0,0.9)"
+          }}
+          closeOnClickOutside={true}
+        />
+      )}
+              
+           <p  onClick={() => openImageViewer(1)} className="text-underline"> 4<sup>th</sup> Year CSE</p>
+           <p onClick={() => openImageViewer(0)} className="text-underline"> 3<sup>rd</sup> Year CSE</p>
+           <p  onClick={() => openImageViewer(2)} className="text-underline"> 2<sup>nd</sup> Year CSE</p>
+           
            
 
           </div>
